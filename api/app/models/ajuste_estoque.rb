b3113303752f401f7ca32870224c3f5051d985c4
Paulo <p_texeira@yahoo.com.br>
2026-01-05 12:18:35 -0300

    Refactor: adição de validação para definir tipo padrão no modelo AjusteEstoque

--- api/app/models/ajuste_estoque.rb
+++ api/app/models/ajuste_estoque.rb
@@ -12,11 +12,17 @@ class AjusteEstoque < ApplicationRecord
             presence: { message: "Quantidade não pode ficar em branco" },
             numericality: { only_integer: true, greater_than: 0, message: "Quantidade deve ser um número inteiro maior que zero" }
 
+  after_initialize :set_tipo_default, if: -> { tipo.nil? }
+
   # Callbacks para atualizar estoque
   after_save :atualizar_estoque
   before_destroy :reverter_estoque
 
   private
+
+  def set_tipo_default
+    self.tipo ||= :entrada
+  end
 
   def atualizar_estoque
     delta = tipo == "entrada" ? quantidade : -quantidade
