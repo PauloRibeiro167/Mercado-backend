5cee512c36bec0af91cabfdb90bb4bca0ae5b3ca
Paulo <p_texeira@yahoo.com.br>
2026-01-05 12:16:51 -0300

    Refactor: melhoria na formatação e organização do controlador de itens de vendas para aumentar a legibilidade do código

--- api/app/controllers/api/v1/item_vendas_controller.rb
+++ api/app/controllers/api/v1/item_vendas_controller.rb
@@ -1,56 +1,51 @@
-module Api
-  module V1
-    class ItemVendasController < ApplicationController
-  before_action :set_item_venda, only: %i[ show update destroy ]
+class Api::V1::ItemVendasController < ApplicationController
+before_action :set_item_venda, only: %i[ show update destroy ]
 
-  # GET /item_vendas
-  def index
-    @item_vendas = ItemVenda.all
+# GET /item_vendas
+def index
+  @item_vendas = ItemVenda.all
 
-    render json: @item_vendas
-  end
-
-  # GET /item_vendas/1
-  def show
-    render json: @item_venda
-  end
-
-  # POST /item_vendas
-  def create
-    @item_venda = ItemVenda.new(item_venda_params)
-
-    if @item_venda.save
-      render json: @item_venda, status: :created, location: @item_venda
-    else
-      render json: @item_venda.errors, status: :unprocessable_content
-    end
-  end
-
-  # PATCH/PUT /item_vendas/1
-  def update
-    if @item_venda.update(item_venda_params)
-      render json: @item_venda
-    else
-      render json: @item_venda.errors, status: :unprocessable_content
-    end
-  end
-
-  # DELETE /item_vendas/1
-  def destroy
-    @item_venda.destroy!
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_item_venda
-      @item_venda = ItemVenda.find(params.expect(:id))
-    end
-
-    # Only allow a list of trusted parameters through.
-    def item_venda_params
-      params.expect(item_venda: [ :venda_id, :lote_id, :desconto, :quantidade, :preco_unitario_vendido ])
-    end
+  render json: @item_vendas
 end
 
+# GET /item_vendas/1
+def show
+  render json: @item_venda
+end
+
+# POST /item_vendas
+def create
+  @item_venda = ItemVenda.new(item_venda_params)
+
+  if @item_venda.save
+    render json: @item_venda, status: :created, location: @item_venda
+  else
+    render json: @item_venda.errors, status: :unprocessable_content
+  end
+end
+
+# PATCH/PUT /item_vendas/1
+def update
+  if @item_venda.update(item_venda_params)
+    render json: @item_venda
+  else
+    render json: @item_venda.errors, status: :unprocessable_content
+  end
+end
+
+# DELETE /item_vendas/1
+def destroy
+  @item_venda.destroy!
+end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_item_venda
+    @item_venda = ItemVenda.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def item_venda_params
+    params.expect(item_venda: [ :venda_id, :lote_id, :desconto, :quantidade, :preco_unitario_vendido ])
   end
 end
