e67ef78ded62bc6b0c82d596e6fd62dff335b075
Paulo <p_texeira@yahoo.com.br>
2026-01-05 12:18:52 -0300

    Refactor: ajuste na formatação das validações no modelo PerfilPermissao para melhorar a legibilidade do código

--- api/app/models/perfil_permissao.rb
+++ api/app/models/perfil_permissao.rb
@@ -9,6 +9,6 @@ class PerfilPermissao < ApplicationRecord
             presence: { message: "Permissão não pode ficar em branco" }
 
   validates_uniqueness_of :permissao_id,
-                         scope: :perfil_id,
-                         message: "Permissão já está associada a este perfil"
+                          scope: :perfil_id,
+                          message: "Permissão já está associada a este perfil"
 end
