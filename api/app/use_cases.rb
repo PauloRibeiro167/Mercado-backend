931257487334e422b189beecdcb7b7a28e9e04c9
Paulo <p_texeira@yahoo.com.br>
2026-01-05 11:28:12 -0300

    Refactor: criação do namespace UseCases para organizar casos de uso

--- api/app/use_cases.rb
+++ api/app/use_cases.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# Namespace raiz para todos os casos de uso da aplicação.
+# @see UseCases::Estoque
 module UseCases
-	# Namespaces para organizacao dos casos de uso
+	# Agrupa casos de uso relacionados a estoque.
 	module Estoque; end
 end
