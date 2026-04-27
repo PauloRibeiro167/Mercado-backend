0862edda78c66e2b09ed9e325057e15a68f70f81
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-04-27 04:13:31 -0300

    Refatoração: Corrige o nome da tabela e reorganiza a definição do método after_commit no modelo PerfilPermissao

--- api/app/models/admin/perfil_permissao.rb
+++ api/app/models/admin/perfil_permissao.rb
@@ -1,18 +1,9 @@
 class Admin::PerfilPermissao < ApplicationRecord
   include Discard::Model if defined?(Discard::Model)
 
-  self.table_name = "perfil_permissoes"
+  self.table_name = "perfil_permissaos"
 
   after_commit :avisar_frontends, on: [ :create, :update ]
-
-  private
-
-  def avisar_frontends
-    ActionCable.server.broadcast("perfil_permissaos_channel", { acao: "atualizado", id: self.id })
-  end
-
-  public
-  self.table_name = "perfil_permissoes"
 
   belongs_to :perfil
   belongs_to :permissao
@@ -26,4 +17,8 @@ class Admin::PerfilPermissao < ApplicationRecord
   validates_uniqueness_of :permissao_id,
                           scope: :perfil_id,
                           message: "Permissão já está associada a este perfil"
+private
+  def avisar_frontends
+    ActionCable.server.broadcast("perfil_permissaos_channel", { acao: "atualizado", id: self.id })
+  end
 end
