4b095657d05e2d07d3bb1eeba93b071735231cf4
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-04-27 03:42:46 -0300

    FEAT: Adiciona injeção de dependência na definição da tabela em Admin::Permissao

--- api/app/models/admin/permissao.rb
+++ api/app/models/admin/permissao.rb
@@ -1,5 +1,5 @@
 class Admin::Permissao < ApplicationRecord
-  has_many :perfil_permissoes, class_name: "PerfilPermissao", dependent: :destroy
+#{injection}  has_many :perfil_permissoes, class_name: "PerfilPermissao", dependent: :destroy
   has_many :perfis, through: :perfil_permissoes
 
   # Validações
@@ -9,15 +9,4 @@ class Admin::Permissao < ApplicationRecord
 
   validates :chave_acao,
             presence: { message: "Chave de ação não pode ficar em branco" },
-            uniqueness: { message: "Chave de ação já está em uso" }
-
-  after_commit :avisar_frontends, on: [:create, :update]
-
-  private
-
-  def avisar_frontends
-    ActionCable.server.broadcast("#{ch}", { acao: "atualizado", id: self.id })
-  end
-
-  public
-end
+            uniqueness: { message: "Chave de ação já está em uso" }end
