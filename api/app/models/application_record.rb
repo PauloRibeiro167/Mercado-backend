268391016c5af6a2f5d81a304566291c4c659c01
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-04-27 03:42:59 -0300

    FEAT: Adiciona método avisar_frontends e configuração de broadcast no ApplicationRecord

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -1,5 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
-#{injection}  primary_abstract_class
+
+  include Discard::Model if defined?(Discard::Model)
+
+  after_commit :avisar_frontends, on: [:create, :update]
+
+  private
+
+  def avisar_frontends
+    ActionCable.server.broadcast("application_records_channel", { acao: "atualizado", id: self.id })
+  end
+
+  public
+  primary_abstract_class
 
   connects_to database: { writing: :primary, reading: :primary_replica }
 end
